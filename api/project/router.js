cfff305a7a20bd705af93e22fa11d97609c4b437
Byungjin-Kim <composerjins@gmail.com>
2023-05-18 14:01:17 -0500

    wip

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,17 @@
 // build your `/api/projects` router here
+const express = require('express');
+// const Project = require('./model');
+
+const router = express.Router();
+
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+        customerMessage: 'something went wrong inside the project router',
+        message: err.message,
+        stack: err.stack,
+    });
+});
+
+
+module.exports = router;
