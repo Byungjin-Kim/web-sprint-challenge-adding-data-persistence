f0f8528bd4d668dd08466cdcfb00d43f200b2558
Byungjin-Kim <composerjins@gmail.com>
2023-05-18 16:20:17 -0500

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,23 @@
 // build your `Project` model here
+function getProjects() {
+    return Promise.resolve('awesome get project!')
+}
+
+// [GET] /api/projects
+
+// Even though project_completed is stored as an integer, the API uses booleans when interacting with the client
+// Example of response body: [{"project_id":1,"project_name":"bar","project_description":null,"project_completed":false}]
+
+
+// createProject
+function createProject() {
+    return Promise.resolve('great post project!')
+}
+
+// [POST] /api/projects
+
+// Even though project_completed is stored as an integer, the API uses booleans when interacting with the client
+// Example of response body: {"project_id":1,"project_name":"bar","project_description":null,"project_completed":false}
+
+
+module.exports = { getProjects, createProject }
--- api/project/router.js
+++ api/project/router.js
@@ -1,17 +1,31 @@
 // build your `/api/projects` router here
 const express = require('express');
-// const Project = require('./model');
+const Project = require('./model');
 
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+    Project.getProjects()
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(next);
+});
+
+router.post('/', (req, res, next) => {
+    Project.createProject(req.body)
+        .then(project => {
+            res.status(201).json(project);
+        })
+        .catch(next);
+});
 
 router.use((err, req, res, next) => { // eslint-disable-line
     res.status(500).json({
-        customerMessage: 'something went wrong inside the project router',
+        customerMessage: 'Something went wrong inside the project router',
         message: err.message,
         stack: err.stack,
     });
 });
-
 
 module.exports = router;
